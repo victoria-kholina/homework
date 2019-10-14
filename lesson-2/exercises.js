825be99d29ac70f8e7ff1904a207f3905e22b6ec
victoria-kholina <kholina.victoria@gmail.com>
2019-10-14 13:08:09 +0300

    home work lesson 2

---
+++ lesson-2/exercises.js
@@ -0,0 +1,31 @@
+// Exersise - 1 
+
+var letters = [];
+var str = "Backend As A Service";
+str = str.split(" ");
+
+for(i=0; i< str.length; i++) {
+letters.push( str[i].substr(0,1) );
+}
+
+str = letters.join("");
+console.log(str);
+document.getElementsByClassName("result-1")[0].innerHTML = str;
+
+// Exersise - 2
+
+checkType (10);
+  
+function checkType (arg) {
+	var date = new Date();
+	date = date.toLocaleString();
+	var result  = document.getElementsByClassName("result-2")[0];
+	if( (typeof(arg) === "number")) {
+		result.innerHTML = date;
+		console.log(date);
+	} else {
+		result.innerHTML = "Неверный тип данных";
+		console.log("Неверный тип данных");
+	}
+}
+	
