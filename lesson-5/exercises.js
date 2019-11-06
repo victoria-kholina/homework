491343dd07d5eb8240d4efe618774ce40ccfb8fb
victoria-kholina <kholina.victoria@gmail.com>
2019-11-06 14:11:18 +0200

    exercises 2,3 lesson 5

--- lesson-5/exercises.js
+++ lesson-5/exercises.js
@@ -15,3 +15,36 @@ testFunc();
 // Exercise - 2
 
 
+function sampleFunc () {
+    console.info ( `Symbols in my code: ${arguments.callee + 0}` )
+}
+
+function modificator ( func ) {
+     func.valueOf = function() {
+  		return func.toString().length;
+ 	}
+}
+
+modificator( sampleFunc )
+
+sampleFunc()
+
+// Exercise - 3
+
+function testArguments () {
+    function generateError ( numArgs ) {
+       throw ( {
+          name: "Application",
+          message: "Invalid arguments",
+          stack: ` Function needs 3 arguments, but only ${numArgs} present`
+    	});
+    }
+    try {
+        arguments.length >= 3 ? null : generateError ( arguments.length )
+    }
+    catch ( err ) {
+        console.error ( `${err.name}: ${err.message}\n${err.stack}` )
+    }
+}
+
+testArguments ( "Google" )
