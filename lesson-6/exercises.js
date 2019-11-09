04d3b25771bd76341544dc9da6dca598fb09c5e0
victoria-kholina <kholina.victoria@gmail.com>
2019-11-09 16:29:21 +0200

    exercise 2,3 lesson 6

--- lesson-6/exercises.js
+++ lesson-6/exercises.js
@@ -4,7 +4,7 @@
 // Вопрос: Какой вариант правильнее из 2-x?
 //Вариант 1
 var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-var classes = ['.first', '.second', '.third', '.fourth', '.fifth', '.sixth'];
+var classes = ['.first', '.second', '.third', '.fourth', '.fifth', '.sixth'];//сразу с точкой
 var style = document.head.appendChild ( document.createElement ( 'style' ) )
 
 style.textContent = `
@@ -15,7 +15,6 @@ style.textContent = `
 `
 for (var i = 0; i< tags.length; i++) {
     var elem = document.createElement(tags[i]);
-	
     document.body.appendChild(elem).innerHTML = `Heading ${tags[i]}`;
     elem.className += classes[i].slice(1, classes[i].length);
 }
@@ -23,7 +22,7 @@ for (var i = 0; i< tags.length; i++) {
 //Вариант 2
 
 var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-var classes = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'];
+var classes = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']; //без точки
 var style = document.head.appendChild ( document.createElement ( 'style' ) )
 var classesForStyle = []
 
@@ -46,7 +45,85 @@ for (var i = 0; i< tags.length; i++) {
 
 // Exercise - 2
 
+var elems = [ 
+    {
+        tagName:"h1",
+        attrs: {
+            className: "first heading",
+            id: "heading-h1",
+            lang: "en",
+            hidden: false,
+            onclick: function(event) {
+                   event.target.innerHTML = `You just clicked on ${this.tagName} !`
+            }
+        }
+        
+    }, 
+    {
+        tagName:"h2",
+        attrs: {
+            className: "second heading",
+            id: "heading-h2",
+            lang: "en",
+            hidden: false,
+            onclick: function(event) {
+                 event.target.innerHTML = `You just clicked on ${this.tagName} !`
+            }
+      }
+        
+    }, 
+    {
+        tagName:"h3",
+        attrs: {
+            className: "third heading",
+            id: "heading-h3",
+            lang: "en",
+            hidden: false,
+            onclick: function(event) {
+                event.target.innerHTML = `You just clicked on ${this.tagName} !`
+            }
+        }
+    }
+];
+
+var style = document.head.appendChild ( document.createElement ( 'style' ) )
+style.textContent = `
+    .heading {
+        padding: 5px;
+        background-color: #ccc;
+    }
+`
+for ( var tag of elems ) {
+    var elem = document.body.appendChild (
+        document.createElement ( tag.tagName )
+    )
+    elem.innerHTML = `Heading ${ tag.tagName }`;
+    for ( var attr in tag.attrs ) {
+         elem [ attr ] = tag.attrs[ attr ]
+    }
+}
 
 
 // Exercise - 3
 
+var tags = [ "header", "footer", "main", "div", "p", "script" ]
+
+var style = document.head.appendChild ( document.createElement ( 'style' ) )
+style.textContent = `
+    .redBack {
+        background-color: red!important;
+    }
+`
+
+for(var i = 0; i < tags.length; i++) {
+    var elem = document.body.appendChild (
+        document.createElement ( tags[i] )
+    ).innerHTML = `This is ${ tags[i] } baby`;
+    var bodyChildren = document.body.childNodes;
+    for (var x = 0; x < bodyChildren.length; x++) {
+        if(bodyChildren[x].nodeName != "SCRIPT") {
+            document.body.childNodes[x].classList.add("redBack");
+        }
+    }
+   
+}
