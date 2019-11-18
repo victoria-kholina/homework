ca0c8893aeaa459423efea48f7c4c96da47c08d3
victoria-kholina <kholina.victoria@gmail.com>
2019-11-18 20:27:10 +0200

    lesson 11 exercise 1 updated

--- lesson-11/exercises.js
+++ lesson-11/exercises.js
@@ -1,14 +1,42 @@
-// Exercise - 1 
+// Exercise - 1
 
+// Вопрос: почему в этом варианте выдало ошибку. Не перезаписало глобальную переменную imageData. В консоле выдало undefined
+
+// var imageData;
+// var request = new XMLHttpRequest;
+// request.onreadystatechange = function (event) {
+//     this.readyState === 4 ? this.status === 200 ?
+//         imageData = JSON.parse(this.responseText) :
+//         console.warn ('request error') :
+//         null
+// }
+// request.open('GET', 'data.json');
+// request.send ();
+// console.log(imageData);
+
+// Рабочий вариант
 var request = new XMLHttpRequest;
 request.onreadystatechange = function (event) {
-    this.readyState === 4 ? this.status === 200 ?
-        console.log (this.responseText) :
-        console.warn ('request error') :
+    if ( this.readyState === 4 ) {
+        if (this.status === 200) {
+           var imageData = JSON.parse(this.responseText);
+           var container = document.getElementById("images")
+            imageData.forEach (
+                image => {
+                    container.appendChild(document.createElement('h2')).innerText = image.title;
+                    container.appendChild(document.createElement('img')).src = image.ref;
+                }
+            )
+        } else {
+            console.warn ('request error')
+        }
+    } else {
         null
+    }
 }
-request.open('GET', 'data.json')
-request.send ()
+request.open('GET', 'data.json');
+request.send ();
+
 
 // Exercise - 2
 
